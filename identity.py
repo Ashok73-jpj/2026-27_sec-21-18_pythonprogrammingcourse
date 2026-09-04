84911b0f16c079c98db0db23199608c493d875b2
Ashok73-jpj <prasannalingala82@gmail.com>
2026-09-04 14:21:53 +0530

    identity operator

---
+++ identity.py
@@ -0,0 +1,15 @@
+a= [1,2,3]
+b=a
+result=b is a #is identity operator
+Result=b is not a 
+print("result of ",b,"is",a,"is:",result)
+print("result of ",b,"is not",a,"is:",Result)
+
+
+
+c=[1,2,3]
+d=[1,2,3]
+result=d is  c #is identity operator
+Result=d is not c
+print("result of ",d,"is",c,"is:",result)
+print("result of ",d,"is not",c,"is:",Result)
